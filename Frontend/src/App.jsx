cafe183a1e6a9b3f4373db3e3ff5b528ed85cfed
Tanmay Deshpande <dsp.tanmay@gmail.com>
2023-09-30 16:53:07 +0530

    modified App.jsx to include map functionality

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,24 +1,26 @@
-import {Routes,Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 // import Navbar from "./components/Navbar"
-import Home from "./pages/Home"
-import { Register } from "./pages/Register"
-import { Login } from "./pages/Login"
-import Education from "./pages/Education"
-import { AuthProvider } from "./context/AuthContext"
-import About from "./pages/About"
+import Home from "./pages/Home";
+import { Register } from "./pages/Register";
+import { Login } from "./pages/Login";
+import Education from "./pages/Education";
+import { AuthProvider } from "./context/AuthContext";
+import About from "./pages/About";
+import Map from "./components/Map";
 
 function App() {
   return (
     <AuthProvider>
       <Routes>
-        <Route path="/login" element={<Login/>} />
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/register" element={<Register/>} />
-        <Route path="/education" element={<Education/>}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/education" element={<Education />} />
+        <Route path="/map" element={<Map />} />
       </Routes>
     </AuthProvider>
-  )
+  );
 }
 
-export default App
+export default App;
